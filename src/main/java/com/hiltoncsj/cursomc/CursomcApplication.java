52dbdfe2df6c5b9b0678fc5de37f175c5f6d9873
Hilton Coutinho <hiltoncsj@gmail.com>
2017-12-18 11:33:21 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/hiltoncsj/cursomc/CursomcApplication.java
+++ src/main/java/com/hiltoncsj/cursomc/CursomcApplication.java
@@ -1,8 +1,7 @@
 package com.hiltoncsj.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
-
-import javax.security.auth.message.config.ClientAuthContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,13 +13,20 @@ import com.hiltoncsj.cursomc.domain.Cidade;
 import com.hiltoncsj.cursomc.domain.Cliente;
 import com.hiltoncsj.cursomc.domain.Endereco;
 import com.hiltoncsj.cursomc.domain.Estado;
+import com.hiltoncsj.cursomc.domain.Pagamento;
+import com.hiltoncsj.cursomc.domain.PagamentoComBoleto;
+import com.hiltoncsj.cursomc.domain.PagamentoComCartao;
+import com.hiltoncsj.cursomc.domain.Pedido;
 import com.hiltoncsj.cursomc.domain.Produto;
+import com.hiltoncsj.cursomc.domain.enums.EstadoPagamento;
 import com.hiltoncsj.cursomc.domain.enums.TipoCliente;
 import com.hiltoncsj.cursomc.repositories.CategoriaRepository;
 import com.hiltoncsj.cursomc.repositories.CidadeRepository;
 import com.hiltoncsj.cursomc.repositories.ClienteRepository;
 import com.hiltoncsj.cursomc.repositories.EnderecoRepository;
 import com.hiltoncsj.cursomc.repositories.EstadoRepository;
+import com.hiltoncsj.cursomc.repositories.PagamentoRepository;
+import com.hiltoncsj.cursomc.repositories.PedidoRepository;
 import com.hiltoncsj.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -38,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -88,6 +98,25 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.save(Arrays.asList(ped1, ped2));
+		pagamentoRepository.save(Arrays.asList(pagto1, pagto2));
+		
+		
+				
 	}
 }
 
--- src/main/java/com/hiltoncsj/cursomc/domain/Cliente.java
+++ src/main/java/com/hiltoncsj/cursomc/domain/Cliente.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
@@ -28,7 +29,7 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-	
+		
 	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
@@ -36,6 +37,9 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 	}
@@ -104,6 +108,14 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -129,6 +141,8 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 	
 	
 	
